fc03360188d1a782d64f03b8ee9e26cf1cc9bc2e
Murali <mkr.raju1991@gmail.com>
2017-02-03 23:12:52 -0700

    refactored_v1

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,16 +1,9 @@
-def insertion(input):
-    l = len(input)
-    count = 0
-    if l>1:
-        n = l-1
-        while n:
-            count+=1
-            i=count-1
-            while i>=0:
-                if input[i+1]<input[i]:
-                    temp = input[i]
-                    input[i] = input[i+1]
-                    input[i+1] = temp
-                i-=1    
-            n-=1
-    return input
+def insertion(data):
+    for i in range(1,len(data)):
+        index = i
+        value = data[i]
+        while i>0 and data[i-1]>value:
+            data[i] = data[i-1]
+            i-=1
+        data[i] = value
+    print data
